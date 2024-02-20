5109bb940e69c2623636d1ae889725d605946d5e
morasjoel80 <134115962+morasjoel80@users.noreply.github.com>
2024-02-20 10:01:33 +0530

    Update DataCollection.py

---
+++ DataCollection.py
@@ -0,0 +1,12 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+cap = cv2.VideoCapture(0)
+detector = HandDetector()
+
+while True:
+    success,img= cap.read()
+    hands,img = detector.findHands(img)
+    cv2.imshow("image",img)
+    cv2.waitKey(1)
+
+
