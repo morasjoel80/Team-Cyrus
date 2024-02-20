fa6dd5f894c33855480b951f4c1a6de06876bfa2
Munchi <148445538+Reo69@users.noreply.github.com>
2024-02-20 10:31:55 +0530

    One Hands Detection

--- DataCollection.py
+++ DataCollection.py
@@ -1,6 +1,7 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
+import math
 
 #   Initialization
 cap = cv2.VideoCapture(0)
@@ -24,6 +25,24 @@ while True:
         imgcrop = img[y - OFFSET: y + h + OFFSET, x - OFFSET: x + w + OFFSET]
 
         aspect_ratio = h/w
+
+        if aspect_ratio > 1:
+            k = IMG_SIZE / h
+            wCal = math.ceil(k * w)
+            imgResize = cv2.resize(imgcrop, (wCal, IMG_SIZE))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((IMG_SIZE - wCal) / 2)
+            imgwhite[:, wGap: wCal + wGap] = imgResize
+
+        else:
+            k = IMG_SIZE / w
+            hCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgcrop, (IMG_SIZE, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((IMG_SIZE - hCal) / 2)
+            imgwhite[hGap: hCal + hGap, :] = imgResize
+
+
         cv2.imshow("Crop", imgcrop)
         cv2.imshow("White", imgwhite)
     cv2.imshow("image", img)
